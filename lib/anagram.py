d6cbb74a42872bc3ce9ad5958df78b2ef9748318
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-02-02 16:24:20 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    pass
+    def __init__(self, word):
+        self.word = word
+    
+    # def getWord(self):
+    #     return self._word
+    
+    # def setWord(self, word):
+    #      self._word = word
+    
+    # word = property(getWord)
+
+    def match(self, words):
+        matched = []
+        for word in words:
+            if sorted(word) == sorted(self.word):
+                matched.append(word)
+        return matched
